22e4b433e4c2b75674ced42b99873a799a7cbc8b
Erin Tan <chimera.mix@gmail.com>
2019-11-17 15:27:36 -0800

    Add files via upload
    edit files from epoch time to current local time, for wsc race record files

---
+++ Time Conversion.py
@@ -0,0 +1,24 @@
+import time
+import datetime
+filename = "Day1-20150720095910.csv"
+#Day1 is from 2015 SCC race
+file = open("Day1note4.txt", "w")
+with open(filename) as f:
+    linenumb = 0
+    for line in f:
+        linenumb = linenumb+1
+        if linenumb == 1:
+            file.write("Date")
+            file.write(",")
+            file.write(line)
+            continue
+        stripline = line.strip()
+        columns = stripline.split(',')
+        print columns[0]
+        my_variable = time.localtime(int(columns[0]))
+        str = time.asctime(my_variable)
+        file.write(str)
+        file.write(",")
+        file.write(stripline)
+        file.write("\n")
+file.close()
